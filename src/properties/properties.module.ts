97c0b3d0c6a30b4643439a9d13c431c9d6e973da
ayshadogo <ayshadahmad@gmail.com>
2026-05-27 14:26:47 +0100

    Property Image Upload

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PropertiesService } from './properties.service';
 import { PropertiesController } from './properties.controller';
+import { PropertyImagesService } from './property-images.service';
+import { PropertyImagesController } from './property-images.controller';
 import { PrismaModule } from '../database/prisma.module';
 import { AuthModule } from '../auth/auth.module';
 import { PropertiesResolver } from './properties.resolver';
@@ -8,16 +11,17 @@ import { PubSub } from 'graphql-subscriptions';
 import { FraudModule } from '../fraud/fraud.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, FraudModule],
-  controllers: [PropertiesController],
+  imports: [PrismaModule, AuthModule, FraudModule, ConfigModule],
+  controllers: [PropertiesController, PropertyImagesController],
   providers: [
     PropertiesService,
+    PropertyImagesService,
     PropertiesResolver,
     {
       provide: 'PUB_SUB',
       useValue: new PubSub(),
     },
   ],
-  exports: [PropertiesService],
+  exports: [PropertiesService, PropertyImagesService],
 })
 export class PropertiesModule {}
